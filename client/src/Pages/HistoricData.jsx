1e253c446c68cd35018acc7e118788050e19ef86
Priydarshini Karanth <kpriya1203@gmail.com>
2023-04-29 14:46:03 +0530

    average chart data calculation function in utils

--- client/src/Pages/HistoricData.jsx
+++ client/src/Pages/HistoricData.jsx
@@ -5,6 +5,7 @@ import { paginate } from '../utils/paginate';
 import TableSection from '../components/TableSection';
 import ExampleBarChart from '../components/Charts/ExampleBarChart';
 import MultiSeriesBarChart from '../components/MultiSeriesBarChart';
+import { calculateAvgData } from '../utils/chartData';
 
 const HistoricData = () => {
   const [dates, setDates] = useState({ startDate: '', endDate: '' });
@@ -50,63 +51,8 @@ const HistoricData = () => {
   };
 
   // calculate chart data
-  const chartData = sensorData.reduce((data, item) => {
-    const { temperature, batteryLevel, timeStamp } = item;
-
-    if (!data[timeStamp]) {
-      data[timeStamp] = {
-        ttlTemperature: temperature,
-        ttlBattery: batteryLevel,
-        count: 1,
-      };
-    } else {
-      data[timeStamp] = {
-        ttlTemperature: data[timeStamp].ttlTemperature + temperature,
-        ttlBattery: data[timeStamp].ttlBattery + batteryLevel,
-        count: data[timeStamp].count + 1,
-      };
-    }
-
-    return data;
-  }, {});
-
-  // calculate average temperature and battery for each day
-  let avgData = [];
-
-  for (const date in chartData) {
-    const avgTemperature = Number(
-      (chartData[date].ttlTemperature / chartData[date].count).toFixed(3)
-    );
-
-    const avgBattery = Number(
-      (chartData[date].ttlBattery / chartData[date].count).toFixed(3)
-    );
-
-    const newValue = {
-      ...chartData[date],
-      date,
-      avgTemperature,
-      avgBattery,
-    };
-
-    avgData.push(newValue);
-  }
-
-  // limit to latest 5
-  avgData.reverse().splice(5);
-  console.log(avgData);
-
-  const categories = avgData.map(({ date }) => {
-    return { label: date };
-  });
-
-  const temperature = avgData.map(({ avgTemperature }) => {
-    return { value: avgTemperature };
-  });
-
-  const batteryLevel = avgData.map(({ avgBattery }) => {
-    return { value: avgBattery };
-  });
+  const { categories, temperature, batteryLevel } =
+    calculateAvgData(sensorData);
 
   return (
     <section className="section">
